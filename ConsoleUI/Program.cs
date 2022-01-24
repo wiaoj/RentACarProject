2459443b05869e2bda2c98360498ffa714ca6e99
wiaoj <71731911+wiaoj@users.noreply.github.com>
2022-01-25 00:52:34 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+CarManager carManager = new(new InMemoryCarDal());
+
+foreach (var car in carManager.GetAll()) {
+    Console.WriteLine($"{car.Id} {car.BrandId} {car.ColorId} {car.Modelyear} {car.DailyPrice} {car.Description}");
+}
